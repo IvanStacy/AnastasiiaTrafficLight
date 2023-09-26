d728ced87a9157c4ff31d6b478513d82c0484efd
Anastasiia Ivanova <anastasiia1ivanova1@gmail.com>
2023-09-26 00:28:49 +0000

    change

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Light from "./light.jsx";
+
 //include images into your bundle
 //create your first component
-const Home = () => {
-	return (
-		<><div className='topStick'></div><div className='body'>
-			<div className='trafficLight'>
-				<Light classList={["red", "light"]} />
-				<Light classList={["yellow", "light"]} />
-				<Light classList={["green", "light"]} />
-			</div>
-		</div></>
-	);
-};
+// const Home = () => {
+// 	return (
+// 		<><div className='topStick'></div><div className='body'>
+// 			<div className='trafficLight'>
+// 				<Light classList={["red", "light"]} />
+// 				<Light classList={["yellow", "light"]} />
+// 				<Light classList={["green", "light"]} />
+// 			</div>
+// 		</div></>
+// 	);
+// };
 
+export function Home() {
+  const [selectedColor, setSelectedColor] = useState();
+  return (
+    <div>
+      <div className="trafficTop"></div>
+      <div className="container">
+        <div
+          className="red light"
+          onClick={() => setSelectedColor("red")}
+          style={{
+            boxShadow: selectedColor === "red" ? "0px 0px 40px 20px #fff" : "",
+          }}
+        ></div>
+        <div
+          className="yellow light"
+          onClick={() => setSelectedColor("yellow")}
+          style={{
+            boxShadow:
+              selectedColor === "yellow" ? "0px 0px 40px 20px #fff" : "",
+          }}
+        ></div>
+        <div
+          className="green light"
+          onClick={() => setSelectedColor("green")}
+          style={{
+            boxShadow:
+              selectedColor === "green" ? "0px 0px 40px 20px #fff" : "",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+}
 export default Home;
--- src/js/component/light.jsx
+++ src/js/component/light.jsx
@@ -1,14 +1,14 @@
 import React,{useState,useEffect} from "react";
 import propTypes from "prop-types";
-const Trafficlight =(props)=>{
-  const classList = props.classList;
-  const [light, selectLight] = useState(classList);
-  useEffect(()=>{
+// const Trafficlight =(props)=>{
+//   const classList = props.classList;
+//   const [light, selectLight] = useState(classList);
+//   useEffect(()=>{
 
-  });
-  return (
-    <div className ={classList.join(" ")} onClick={() => selectLight()}></div>
-  );
-};
+//   });
+//   return (
+//     <div className ={classList.join(" ")} onClick={() => selectLight()}></div>
+//   );
+// };
 
-export default Trafficlight;
+// export default Trafficlight;
